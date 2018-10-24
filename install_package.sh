d296f46271c1d4b5e366786eaa6cae599bbd9611
Ralf Waspe <ralf@waspe.de>
2018-10-24 16:24:46 +0200

    local developer install

--- install_package.sh
+++ install_package.sh
@@ -5,7 +5,7 @@ PACK_DIR=/home/waspe/.local/lib/python3.5/site-packages
 rm -rf $PACK_DIR/wapy*
 
 echo
-/usr/bin/env python3 -m pip install --user .
+/usr/bin/env python3 -m pip install  -e .  --user
 
 echo 
 ls -1 $PACK_DIR | grep wapy
---
+++ install_package_apache.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo /usr/bin/env python3 -m pip install . 
