3ec83ced74024eda11103f37dec68b2cb93dbdbd
Ralf Waspe <ralf@waspe.de>
2018-10-24 16:02:32 +0200

    structure for package pip install

--- install_package.sh
+++ install_package.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+PACK_DIR=/home/waspe/.local/lib/python3.5/site-packages
+
+rm -rf $PACK_DIR/wapy*
+
+echo
 /usr/bin/env python3 -m pip install --user .
+
+echo 
+ls -1 $PACK_DIR | grep wapy
--- setup.py
+++ setup.py
@@ -2,21 +2,13 @@
 
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="wapy",
     version="0.0.1",
     author="Ralf Waspe",
     author_email="ralf@waspe.de",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    license='MIT',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    zip_safe=False,
 )
