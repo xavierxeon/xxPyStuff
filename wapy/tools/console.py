ea2a48bfe20154cec1f3435a916aa6fb77f6f276
Ralf Waspe <raw@mmmi.sdu.dk>
2018-11-13 15:07:58 +0100

    test differrent Console outouts

--- wapy/tools/console.py
+++ wapy/tools/console.py
@@ -1,43 +1,59 @@
 ﻿#!/usr/bin/env python3
 
 class Console:
-    
+
+    class _ColorBash:
+        Clear = '\033[0m'
+        Bold = '\033[1m'
+        Grey = '\033[90m'
+        Red = '\033[91m'
+        Green = '\033[92m'
+        Yellow = '\033[93m'
+        Blue = '\033[94m'
+        Magenta = '\033[95m'
+        Cyan = '\033[96m'
+        White = '\033[97m'
+
+    Color = None
+
     @staticmethod
     def _write(message, bold, color):
+        if not Console.Color:
+            Console.Color = Console._ColorBash
+            
         if bold:
-            return '\033[1m' + color + message + '\033[0m'    
+            return Console.Color.Bold + color + message + Console.Color.Clear    
         else:
-            return color + message + '\033[0m'    
+            return color + message + Console.Color.Clear    
 
     @staticmethod
     def grey(message, bold = False):
-        return Console._write(message, bold, '\033[90m')
+        return Console._write(message, bold, Console.Color.Grey)
 
     @staticmethod
     def red(message, bold = False):
-        return Console._write(message, bold, '\033[91m')
+        return Console._write(message, bold, Console.Color.Red)
 
     @staticmethod
     def green(message, bold = False):
-        return Console._write(message, bold, '\033[92m')        
+        return Console._write(message, bold, Console.Color.Green)        
 
     @staticmethod
     def yellow(message, bold = False):
-        return Console._write(message, bold, '\033[93m')
+        return Console._write(message, bold, Console.Color.Yellow)
 
     @staticmethod
     def blue(message, bold = False):
-        return Console._write(message, bold, '\033[94m')
+        return Console._write(message, bold, Console.Color.Blue)
             
     @staticmethod
     def magenta(message, bold = False):
-        return Console._write(message, bold, '\033[95m')
+        return Console._write(message, bold, Console.Color.Magenta)
 
     @staticmethod
     def cyan(message, bold = False):
-        return Console._write(message, bold, '\033[96m')
+        return Console._write(message, bold, Console.Color.Cyan)
 
     @staticmethod
     def white(message, bold = False):
-        return Console._write(message, bold, '\033[97m')  
-
+        return Console._write(message, bold, Console.Color.White)  
