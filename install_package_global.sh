ca61532aa5ba62c57665fc7f6cad2580abb31a9c
Ralf Waspe <ralf@waspe.de>
2018-12-10 08:34:35 +0100

    updated setup

--- install_package_global.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo /usr/bin/env python3 -m pip uninstall -y wapy
-sudo /usr/bin/env python3 -m pip install git+https://github.com/xavierxeon/PythonPackage.git
--- setup.py
+++ setup.py
@@ -2,14 +2,21 @@
 
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="wapy",
     version="0.0.5",
     author="Ralf Waspe",
-    author_email="ralf@waspe.de",
+    author_email="rwaspe@me.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xavierxeon/PythonPackage",
     license='MIT',
     packages=setuptools.find_packages(),
-    install_requires=['colorama', 'pyside2'],
+    install_requires=['colorama', 'pyside2', 'urwid'],
     include_package_data=True,
     zip_safe=False,
 )
