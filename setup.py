78ca4a1529effc6c541e4c0f60b2579e7197ee2a
Ralf Waspe <rwaspe@me.com>
2019-02-02 11:23:04 +0100

    changed dir name

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+packages = setuptools.find_packages()
+
 setuptools.setup(
     name="xxpystuff",
-    version="0.0.6",
+    version="0.0.7",
     author="Ralf Waspe",
     author_email="rwaspe@me.com",
-    description="A small example package",
+    description="A collection of python tools",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/xavierxeon/xxPyStuff",
     license='MIT',
-    packages=setuptools.find_packages(),
+    packages=packages,
     install_requires=['colorama', 'pyside2', 'urwid'],
     include_package_data=True,
     zip_safe=False,
