69c22dda27e72206a564c685b76d49ab91228f99
Ralf Waspe <raw@mmmi.sdu.dk>
2019-06-11 09:35:10 +0200

    process status indicator

--- xxpystuff/tools/process.py
+++ xxpystuff/tools/process.py
@@ -1,17 +1,18 @@
 ﻿#!/usr/bin/env python3
 
 import os
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, TimeoutExpired
 
 class Process:
 
-    def __init__(self, command):
+    def __init__(self, command, statusIndicator = None):
 
         self.workDir = None
         self.outputFunction = None
         self.errorFunction = None
 
         self._command = command
+        self._statusIndicator = statusIndicator
         self._handle = None
 
     def start(self, *argList):
@@ -25,20 +26,34 @@ class Process:
             content.append(str(arg))
 
         self._handle = Popen(content, stdout = PIPE, stderr = PIPE, stdin = PIPE, bufsize = 1, universal_newlines = True)
-        while self._handle.poll() is None:
-            self._capture(self._handle.stdout, self.outputFunction)
-            self._capture(self._handle.stderr, self.errorFunction)
-                    
-        self._capture(self._handle.stdout, self.outputFunction)
-        self._capture(self._handle.stderr, self.errorFunction)
+        success = False
+        while True:
+            try:
+                output, error = self._handle.communicate(None, 0.2)
+
+                # timeout not reached, therefore process is finished
+                if output and self.outputFunction:
+                    self.outputFunction(output)
+                if error and self.errorFunction:
+                    self.errorFunction(error)
+                if not error:
+                    success = True
+                break 
+            except TimeoutExpired:
+                if self._statusIndicator:
+                    self._statusIndicator.busy()
 
         self.stop()
         os.chdir(current)
+
+        return success
 
     def stop(self):
 
         if not self._handle:
             return
+
+        self._handle.kill()
 
         del self._handle
         self._handle = None
@@ -70,7 +85,7 @@ class Process:
             return
 
         while True:
-            output = pipe.readline().strip()
+            output = pipe.read()
             if not output:
                 return
             callBack(output)
