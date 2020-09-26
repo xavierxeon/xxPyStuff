74217e35857170de901548c7dad0a4c5b69cea20
Ralf Waspe <rwaspe@me.com>
2020-09-26 14:30:45 +0200

    format

--- xxpystuff/tools/process.py
+++ xxpystuff/tools/process.py
@@ -5,89 +5,89 @@ from subprocess import Popen, PIPE, TimeoutExpired
 
 class Process:
 
-    def __init__(self, command, statusIndicator = None):
+   def __init__(self, command, statusIndicator = None):
 
-        self.workDir = None
-        self.outputFunction = None
-        self.errorFunction = None
+      self.workDir = None
+      self.outputFunction = None
+      self.errorFunction = None
 
-        self._command = command
-        self._statusIndicator = statusIndicator
-        self._handle = None
+      self._command = command
+      self._statusIndicator = statusIndicator
+      self._handle = None
 
-    def startWithArguments(self, *arguments):
+   def startWithArguments(self, *arguments):
 
-        content = [ self._command ]
-        for arg in arguments:
-            content.append(str(arg))
+      content = [ self._command ]
+      for arg in arguments:
+         content.append(str(arg))
 
-        self._executeInternal(content)
+      self._executeInternal(content)
 
-    def start(self, argList = None):
+   def start(self, argList = None):
 
-        content = [ self._command ]
-        if argList:
-            for arg in argList:
-                content.append(str(arg))
+      content = [ self._command ]
+      if argList:
+         for arg in argList:
+               content.append(str(arg))
 
-        self._executeInternal(content)
+      self._executeInternal(content)
 
-    def stop(self):
+   def stop(self):
 
-        if not self._handle:
-            return
+      if not self._handle:
+         return
 
-        self._handle.kill()
+      self._handle.kill()
 
-        del self._handle
-        self._handle = None
+      del self._handle
+      self._handle = None
 
-    def write(self, text):
+   def write(self, text):
 
-        pass
+      pass
 
-    @staticmethod
-    def execute(command, workDir = None):
+   @staticmethod
+   def execute(command, workDir = None):
 
-        current = os.getcwd()
-        if workDir:
-            os.chdir(workDir)
+      current = os.getcwd()
+      if workDir:
+         os.chdir(workDir)
 
-        with Popen(command, stdout = PIPE, stderr = PIPE) as process:
-            output, error = process.communicate()
+      with Popen(command, stdout = PIPE, stderr = PIPE) as process:
+         output, error = process.communicate()
 
-        os.chdir(current)
+      os.chdir(current)
 
-        if error:
-            return error.decode('ascii')
-        else:
-            return output.decode('ascii')
-            
-    def _executeInternal(self, content):
+      if error:
+         return error.decode('ascii')
+      else:
+         return output.decode('ascii')
+         
+   def _executeInternal(self, content):
 
-        current = os.getcwd()
-        if self.workDir:
-            os.chdir(self.workDir)
+      current = os.getcwd()
+      if self.workDir:
+         os.chdir(self.workDir)
 
-        self._handle = Popen(content, stdout = PIPE, stderr = PIPE, stdin = PIPE, bufsize = 1, universal_newlines = True)
-        success = False
-        while True:
-            try:
-                output, error = self._handle.communicate(None, 0.2)
+      self._handle = Popen(content, stdout = PIPE, stderr = PIPE, stdin = PIPE, bufsize = 1, universal_newlines = True)
+      success = False
+      while True:
+         try:
+            output, error = self._handle.communicate(None, 0.2)
 
-                # timeout not reached, therefore process is finished
-                if output and self.outputFunction:
-                    self.outputFunction(output) #pylint: disable=not-callable
-                if error and self.errorFunction:
-                    self.errorFunction(error) #pylint: disable=not-callable
-                if not error:
-                    success = True
-                break 
-            except TimeoutExpired:
-                if self._statusIndicator:
-                    self._statusIndicator.busy()
+            # timeout not reached, therefore process is finished
+            if output and self.outputFunction:
+               self.outputFunction(output) #pylint: disable=not-callable
+            if error and self.errorFunction:
+               self.errorFunction(error) #pylint: disable=not-callable
+            if not error:
+               success = True
+            break 
+         except TimeoutExpired:
+            if self._statusIndicator:
+               self._statusIndicator.busy()
 
-        self.stop()
-        os.chdir(current)
+      self.stop()
+      os.chdir(current)
 
-        return success
+      return success
